164be4ed7a5e7db770349357c2ecf8ce77d48afd
Abdel <113614023+TAHIRI1985@users.noreply.github.com>
2023-01-08 02:29:49 +0100

    ....

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -26,18 +26,20 @@ class PostsController extends AppController
     public function index()
     {
         $articles = $this->Article->last();
-        $categories = $this->Categorie->all();
 
-        $this->render('posts.index', compact('articles', 'categories'));
+        $items = $this->Categorie->all();
+
+        $this->render('posts.index', compact('articles',  'items'));
     }
 
 
 
     public function article()
     {
+        $items = $this->Categorie->all();
         $categories = $this->Categorie->all();
         $article = $this->Article->findWithCategory($_GET['id']);
-        $this->render('posts.article', compact('article', 'categories'));
+        $this->render('posts.article', compact('article', 'categories', 'items'));
     }
 
 
@@ -53,11 +55,11 @@ class PostsController extends AppController
             $this->notFound();
         }
 
-
+        $items = $this->Categorie->all();
         $articles = $this->Article->lastByCategorie($_GET['id']);
         $categories = $this->Categorie->all();
         // App::setTitle($categorie->nom_Categorie);
-        $this->render('posts.categorie', compact('articles', 'categories', 'categorie'));
+        $this->render('posts.categorie', compact('articles', 'categories', 'categorie', 'items'));
     }
 
     public function login()
--- essai.php
+++ essai.php
@@ -4,3 +4,17 @@
     <?= $form->input('pss_Client', 'Mot de passe'), ['type' => 'password'] ?>
     <button class="btn btn-primary">Envoyer</button>
 </form>
+<!-- /////////////////////////////////////////-->
+
+<?= $form->input('nom_Article', 'Titre '); ?>
+<?= $form->input('Quantité', 'Quantité '); ?>
+
+<?= $form->input('description_Article', 'Description '); ?>
+
+<?= $form->input('prix_Article', 'Prix '); ?>
+<?= $form->input('image1_Article', 'Image1 ', ['type' => 'file']); ?>
+<?= $form->input('image2_Article', 'Image2 ', ['type' => 'file']); ?>
+<?= $form->select('id_categories', 'Catégorie ', $categories); ?>
+
+
+<button type="submit" class="btn btn-primary" name="edit_submit">Confirmer</button>
